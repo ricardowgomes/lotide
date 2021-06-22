9197347009823c6dd2019058000bd4df6491bbda
Ricardo W. Gomes <ricardowgomes@outlook.com>
2021-06-22 23:38:44 +0000

    added const assertArraysEqual = (arr1, arr2) => {   let output;
      for (let i = 0; i < arr1.length; i++) {
        if (arr1[i] === arr2[i]) {
          output = true;
    
        } else {
          output = false;
          break;
        }
    
      } (output ? console.log() : console.log());
    };
    
    assertArraysEqual([1, 2, 3], [1, 2, 3]) // => should pass
    assertArraysEqual([1, 2, 3], [1, 2, 4]) // => should fail
    assertArraysEqual([1, 2, 3, 5, hi!], [1, 2, 3, 5, hi!]) // => should pass
    assertArraysEqual([1, 2, 3], [1, 2, 3]) // => should fai
    
    git branch
    
    .exit
    logout
    git cancel

---
+++ assertArraysEqual.js
@@ -0,0 +1,19 @@
+const assertArraysEqual = (arr1, arr2) => {
+  let output;
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] === arr2[i]) {
+      output = true;
+
+    } else {
+      output = false;
+      break;
+    }
+
+  } (output ? console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`) : console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`));
+};
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]) // => should pass
+assertArraysEqual([1, 2, 3], [1, 2, 4]) // => should fail
+assertArraysEqual([1, 2, 3, 5, "hi!"], [1, 2, 3, 5, "hi!"]) // => should pass
+assertArraysEqual([1, "2", 3], [1, 2, 3]) // => should fail
